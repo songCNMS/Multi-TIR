fc2e0504f6da4c6577aabf1b29f8f046cf677de4
Your Name <you@example.com>
2025-11-05 00:44:03 -0800

    u

--- start_baichuan_API.sh
+++ start_baichuan_API.sh
@@ -1,8 +1,7 @@
 devices=$1
 tensor_parallel_size=$2
-
-uv venv vllmenv -p 3.10
+uv venv vllmenv -p 3.10 --clear
 source vllmenv/bin/activate
-uv pip install -U vllm=0.9.0
+uv pip install -U vllm==0.9.0
 uv pip install -U "transformers!=4.52.0,<=4.52.4,>=4.49.0"
 CUDA_VISIBLE_DEVICES=$devices vllm serve baichuan-inc/Baichuan-M2-32B --trust-remote-code --tensor-parallel-size $tensor_parallel_size --gpu-memory-utilization 0.95
