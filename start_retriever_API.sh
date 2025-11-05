464dc9e6904143c37f581034c164b6a119cc41f2
Your Name <you@example.com>
2025-11-04 22:59:29 -0800

    u

--- start_retriever_API.sh
+++ start_retriever_API.sh
@@ -1,11 +1,15 @@
 # Setup Search API environment
-conda create -n retriever python=3.10 -y
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate retriever
+# conda create -n retriever python=3.10 -y
+# source ~/anaconda3/etc/profile.d/conda.sh
+# conda activate retriever
+uv venv retrieverenv -p 3.10 --clear
+source retrieverenv/bin/activate
 conda install -y pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.1 -c pytorch -c nvidia
-pip install transformers datasets pyserini omegaconf
-conda install -y -c pytorch -c nvidia faiss-gpu=1.8.0
-pip install uvicorn fastapi tqdm
+uv pip install transformers datasets pyserini omegaconf
+# conda install -y -c pytorch -c nvidia faiss-gpu=1.8.0
+uv pip install faiss-gpu-cu12
+uv pip install accelerate
+uv pip install uvicorn fastapi tqdm
 cd Search_R1/search_r1/search
 bash build_index_symptoms.sh
 bash build_index.sh
@@ -13,5 +17,5 @@ cd ../../..
 cd Search_R1
 bash retrieval_launch.sh &
 bash retrieval_launch_symptoms.sh &
-bash retrieval_logprobs_launch.sh &
+# bash retrieval_logprobs_launch.sh &
 sleep infinity
