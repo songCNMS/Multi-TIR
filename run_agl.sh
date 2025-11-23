159a6144ca6c800a74b0454c08d0fe8e177acf19
Your Name <you@example.com>
2025-11-23 05:45:09 -0800

    agl

---
+++ run_agl.sh
@@ -0,0 +1,24 @@
+cd agent-lightning
+cd dashboard
+npm ci
+npm run build
+cd ..
+pip install uv
+uv sync --frozen \
+    --extra apo \
+    --extra verl \
+    --group dev \
+    --group torch-gpu-stable \
+    --group trl \
+    --group agents \
+    --no-default-groups
+cd ..
+uv pip install -r PIKE_RAG/requirements.txt
+# uv pip install -U agentlightning[verl]
+uv pip install vllm==0.10.2
+uv pip install verl==0.5.0
+uv pip install flash-attn --no-build-isolation
+uv pip install click==8.2.1
+uv pip install -U torch==2.8.0 torchvision
+cd PIKE_RAG
+python train_search_agent.py --llm-proxy --model Qwen/Qwen3-4B-Instruct-2507 2>&1 | tee agent_train.log
