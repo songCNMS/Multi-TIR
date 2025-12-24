4b7bb0d50bcbfa894e908a9915d50f0d25663083
Lei Song <lesong@microsoft.com>
2025-12-23 20:50:14 -0800

    u

--- run_agl.sh
+++ run_agl.sh
@@ -3,39 +3,46 @@ mv env_configs/.env_amlt env_configs/.env
 
 bash search_r1/start_retriever.sh true
 
+
 if $3; then
     echo "Dense reward is ON"
-    bash start_summary_api.sh
+    bash start_summary_api.sh Qwen/Qwen3-4B-Instruct-2507 0,1 &
 else
     echo "Dense reward is OFF"
 fi
 
 cd ..
-cd agent-lightning
-cd dashboard
-npm ci
-npm run build
-cd ..
-pip install uv
-uv sync --frozen \
-    --extra apo \
-    --extra verl \
-    --group dev \
-    --group torch-gpu-stable \
-    --group trl \
-    --group agents \
-    --no-default-groups
-cd ..
-source agent-lightning/.venv/bin/activate
-uv pip install -r PIKE_RAG/requirements.txt
-# uv pip install -U agentlightning[verl]
-uv pip install vllm==0.10.2
-uv pip install verl==0.5.0
-uv pip install flash-attn --no-build-isolation
-uv pip install click==8.2.1
-uv pip install -U torch==2.8.0 torchvision
-uv pip install jsonlines
-uv pip install -U "numpy<2.0"
+
+if $4; then
+    echo "Setting up Agent-Lightning environment..."
+    cd agent-lightning
+    cd dashboard
+    npm ci
+    npm run build
+    cd ..
+    pip install uv
+    uv sync --frozen \
+        --extra apo \
+        --extra verl \
+        --group dev \
+        --group torch-gpu-stable \
+        --group trl \
+        --group agents \
+        --no-default-groups
+    cd ..
+    source agent-lightning/.venv/bin/activate
+    uv pip install -r PIKE_RAG/requirements.txt
+    # uv pip install -U agentlightning[verl]
+    uv pip install vllm==0.10.2
+    uv pip install verl==0.5.0
+    uv pip install flash-attn --no-build-isolation
+    uv pip install click==8.2.1
+    uv pip install -U torch==2.8.0 torchvision
+    uv pip install jsonlines
+    uv pip install -U "numpy<2.0"
+else
+    source agent-lightning/.venv/bin/activate
+fi
 
 cd PIKE_RAG
 export NCCL_SHM_DISABLE=1
@@ -44,4 +51,8 @@ export NCCL_P2P_DISABLE=1
 # python train_search_agent.py --llm-proxy --model /mnt/storage/data/search/logs/sft-tool-calling/Qwen/Qwen3-4B-Instruct-2507-r16-alpha16 --n-gpus 8 2>&1 | tee agent_train.log
 # python train_search_agent.py --llm-proxy --model Qwen/Qwen3-4B-Instruct-2507 --n-gpus 8 2>&1 | tee agent_train.log
 
-python train_search_agent.py --llm-proxy --model $1 --n-gpus $2 2>&1 | tee agent_train.log
+if $3; then
+    DENSE_REWARD_ON=true python train_search_agent.py --llm-proxy --model $1 --n-gpus $2 2>&1 | tee agent_train.log
+else
+    python train_search_agent.py --llm-proxy --model $1 --n-gpus $2 2>&1 | tee agent_train.log
+fi
--- start_retriever_API.sh
+++ start_retriever_API.sh
@@ -2,24 +2,28 @@
 # conda create -n retriever python=3.10 -y
 # source ~/anaconda3/etc/profile.d/conda.sh
 # conda activate retriever
-echo "Setting up Retriever API environment..."
-uv venv retrieverenv -p 3.10 --clear
-source retrieverenv/bin/activate
-conda install -y pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.1 -c pytorch -c nvidia
-uv pip install transformers datasets pyserini omegaconf
-# conda install -y -c pytorch -c nvidia faiss-gpu=1.8.0
-# conda install -c conda-forge faiss-gpu
-uv pip install faiss-cpu
-uv pip install accelerate
-uv pip install uvicorn fastapi tqdm
-uv pip install -U "numpy<2.0"
-cd Search_R1/search_r1/search
-bash build_index_symptoms.sh
-bash build_index.sh
-cd ../../..
+if $2; then
+    echo "Setting up Retriever API environment..."
+    uv venv retrieverenv -p 3.10 --clear
+    source retrieverenv/bin/activate
+    conda install -y pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+    uv pip install transformers datasets pyserini omegaconf
+    # conda install -y -c pytorch -c nvidia faiss-gpu=1.8.0
+    # conda install -c conda-forge faiss-gpu
+    uv pip install faiss-cpu
+    uv pip install accelerate
+    uv pip install uvicorn fastapi tqdm
+    uv pip install -U "numpy<2.0"
+    cd Search_R1/search_r1/search
+    bash build_index_symptoms.sh
+    bash build_index.sh
+    cd ../../..
+else
+    source retrieverenv/bin/activate
+fi
 cd Search_R1
 echo "Launching Retriever API servers..."
 bash retrieval_launch.sh $1 &
 bash retrieval_launch_symptoms.sh $1 &
 # bash retrieval_logprobs_launch.sh &
-sleep 120
+# sleep 120
