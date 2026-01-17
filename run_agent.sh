0f9ec5fd039b943adae3cf1833d6febe4f9d1e7b
Lei Song <lesong@microsoft.com>
2026-01-17 06:31:33 -0800

    u

--- run_agent.sh
+++ run_agent.sh
@@ -1,9 +1,42 @@
+#!/bin/bash
 
-seed=$1
-cd PIKE_RAG
-uv venv -p 3.10 --clear
-source .venv/bin/activate
-uv pip install -r requirements.txt
-python prompt_templates.py
-python agent.py exp_name=data_collection seed=$seed task=search mode=train iterations=5 
+
+set +x
+# SFT + GRPO
+# 当前时间
+TIME=$(date +%Y%m%d_%H%M%S)
+
+
+cp PIKE_RAG/env_configs/.env_data PIKE_RAG/env_configs/.env
+
+bash start_eval_model.sh 0,1,2,3,4,5,6,7 8 Qwen/Qwen3-4B-Instruct-2507 true
+cd agent-lightning
+cd dashboard
+npm ci
+npm run build
 cd ..
+pip install uv
+uv sync --frozen \
+    --extra apo \
+    --extra verl \
+    --group dev \
+    --group torch-gpu-stable \
+    --group trl \
+    --group agents \
+    --no-default-groups
+cd ..
+source agent-lightning/.venv/bin/activate
+uv pip install -r PIKE_RAG/requirements.txt
+# uv pip install -U agentlightning[verl]
+uv pip install vllm==0.10.2
+uv pip install verl==0.5.0
+uv pip install flash-attn --no-build-isolation
+uv pip install click==8.2.1
+uv pip install -U torch==2.8.0 torchvision
+uv pip install jsonlines
+uv pip install -U "numpy<2.0"
+cd PIKE_RAG
+bash search_r1/start_retriever.sh true
+sleep 30
+python agent.py exp_name=data_collection_gptnew seed=$1 task=search mode=train iterations=5  | tee agent_train.log
+sleep infinity
--- run_agl_dist.sh
+++ run_agl_dist.sh
@@ -49,7 +49,7 @@ if [ "$LOCAL_IP" == "$HEAD_IP" ]; then
     uv pip install -U "numpy<2.0"
     cd PIKE_RAG
     bash search_r1/start_retriever.sh true
-    DENSE_REWARD_ON=$3 python train_search_agent.py --llm-proxy --model $1 --n-gpus $2 --exp-name $4 2>&1 | tee agent_train.log
+    FINAL_ANSWER_MODEL=gpt TOOL_CALL_DENSE_REWARD_ON=false DENSE_REWARD_ON=false python train_search_agent.py --llm-proxy --model $1 --n-gpus $2 --exp-name $3 2>&1 | tee agent_train.log
     sleep infinity
 elif [ "$LOCAL_IP" == "node-1" ]; then
     cd PIKE_RAG; bash start_summary_api.sh Qwen/Qwen3-4B-Instruct-2507 0,1,2,3,4,5,6,7
@@ -82,8 +82,9 @@ else
     uv pip install -U "numpy<2.0"
     cd PIKE_RAG
     bash search_r1/start_retriever.sh true
-    TOOL_CALL_DENSE_REWARD_ON=false DENSE_REWARD_ON=false python train_search_agent.py --llm-proxy --model $1 --n-gpus $2 --exp-name wot_ds 2>&1 | tee agent_train.log
+    python agent_data_traj.py task=search iter=0
+    FINAL_ANSWER_MODEL=gpt TOOL_CALL_DENSE_REWARD_ON=true DENSE_REWARD_ON=false python train_search_agent.py --llm-proxy --model $1 --n-gpus $2 --exp-name $3 2>&1 | tee agent_train.log
     sleep infinity
 fi
 
-# TOOL_CALL_DENSE_REWARD_ON=false DENSE_REWARD_ON=false python train_search_agent.py --llm-proxy --model Qwen/Qwen3-4B-Instruct-2507 --n-gpus 8 --exp-name wot_ds 2>&1 | tee agent_train.log
+# FINAL_ANSWER_MODEL=gpt TOOL_CALL_DENSE_REWARD_ON=false DENSE_REWARD_ON=true python train_search_agent.py --llm-proxy --model Qwen/Qwen3-4B-Instruct-2507 --n-gpus 8 --exp-name fa-gpt_tr-off_rm-on 2>&1 | tee agent_train.log
